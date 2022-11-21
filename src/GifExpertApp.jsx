a2aef173d7c96a9cdbc44b0124529e408ccda77c
andrespizarro2021 <andresalejandropizarrogodoy@gmail.com>
2022-11-21 12:24:36 -0300

    primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useState } from 'react'
+import AddCategory from './components/AddCategory'
+import GifGrid from './components/GifGrid'
+
+const GifExpertApp = () => {
+    const [categories, setCategories] = useState(['One Punch', 'Dragon Ball'])
+
+    const onAddCategory = (newCategory) => {
+
+        if ( categories.includes(newCategory)) return;
+
+           setCategories([ newCategory, ...categories]);
+    }
+
+  return (
+    <>
+    <h1>GifExpertApp</h1>
+
+    <AddCategory 
+    
+        onNewCategory={ (value) => onAddCategory(value)}
+
+        />
+
+   
+          { 
+            categories.map((category) => (
+            <GifGrid 
+                key= {category}  
+                category={category}/>
+
+           ))
+         }
+
+
+       
+    </>
+  )
+}
+
+export default GifExpertApp
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+const AddCategory = ({onNewCategory}) => {
+
+    const [inputValue, setInputValue] = useState('One Punch')
+
+    const onInputChange = ({target}) =>{
+        
+        setInputValue(target.value)
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault()
+        if ( inputValue.trim().length <= 1) return;
+        //setCategories( categories => [ inputValue, ...categories])
+        onNewCategory( inputValue.trim())
+        setInputValue(' ')
+    }
+
+  return (
+    <form onSubmit={(event) => onSubmit(event)}>
+    <input
+        type="text"
+        placeholder="Buscar"
+        value={ inputValue}
+        onChange={ onInputChange}
+  />
+  </form>
+  )
+}
+
+
+export default AddCategory
